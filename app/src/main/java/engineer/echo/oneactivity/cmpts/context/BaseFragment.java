98f3b9a1640a93a5b03daff1ce0c9d509bcbdbd7
plucky <plucky.pan@ubnt.com>
2018-01-07 01:14:26 +0800

    添加BaseFragment

---
+++ app/src/main/java/engineer/echo/oneactivity/cmpts/context/BaseFragment.java
@@ -0,0 +1,13 @@
+package engineer.echo.oneactivity.cmpts.context;
+
+import engineer.echo.oneactivity.core.MasterFragment;
+
+/**
+ * BaseFragment
+ * Created by Plucky<plucky@echo.engineer> on 2018/1/6 下午11:03.
+ * more about me: http://www.1991th.com
+ */
+
+public class BaseFragment extends MasterFragment {
+    public final String TAG = getClass().getSimpleName();
+}
