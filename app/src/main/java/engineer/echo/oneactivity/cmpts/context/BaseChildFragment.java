64092627ae2bd0e2f89e22963f8b9ff83f6e0a4d
plucky <plucky.pan@ubnt.com>
2018-01-08 00:36:22 +0800

    添加子Fragment的继承关系

---
+++ app/src/main/java/engineer/echo/oneactivity/cmpts/context/BaseChildFragment.java
@@ -0,0 +1,13 @@
+package engineer.echo.oneactivity.cmpts.context;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * BaseFragment
+ * Created by Plucky<plucky@echo.engineer> on 2018/1/6 下午11:03.
+ * more about me: http://www.1991th.com
+ */
+
+public class BaseChildFragment extends Fragment {
+    public final String TAG = getClass().getSimpleName();
+}
